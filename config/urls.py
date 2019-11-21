80a4ee95570c0409c49a14373589bc873ff3141b
syunsuke <syunsuk1024@gmail.com>
2019-11-22 01:06:39 +0900

    2019-11-22-1:00

--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('fukushu',include("fukushu.urls")),
-    path('kounyu',include('kounyu.urls')),
+    path('',include('kounyu.urls')),
 ]
